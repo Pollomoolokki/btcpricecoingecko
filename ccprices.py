bc7231d8c2627c2fd4ac9fdee68adb5a2deb474c
pollomoolokki <pollomoolokki@gmail.com>
2020-12-22 17:13:23 +0200

    refaktoroidaan lisaa uuteen oksaan. kommentoitu ja vaihdettu logiikkaa

--- ccprices.py
+++ ccprices.py
@@ -3,24 +3,27 @@ import json
 from datetime import datetime
 import os
 
-cg = CoinGeckoAPI()
-t = datetime.now()
-t_string = t.strftime("%d/%m/%Y %H:%M:%S")
-bitcoin = "bitcoin"
-litecoin = "litecoin"
+cg = CoinGeckoAPI() #tehdaan cg apista
+t = datetime.now() #otetaan aika
+t_string = t.strftime("%d/%m/%Y %H:%M:%S") #formatoidaan aika
+#trackedcoins = ["bitcoin", "litecoin", "iota"]
+#bitcoin = "bitcoin"
+#litecoin = "litecoin"
 usd = "usd"
+#writecoins -aliohjelma kirjoittaa mahd. kolikot lyhenteina apicoins.txt tiedostoon
+#ei tulosta mitaan jos txt olemassa
 def writecoins():
     coins = open("apicoins.txt", "a")
     #done = open("coinsalreadywritten.txt", "a")
     if(os.path.getsize("apicoins.txt") > 0):
         coins.close()
-        donothing = 0
-        #coinsalreadywritten.write("")
     else:
         coins.write(str(cg.get_supported_vs_currencies()))
         coins.close()
+#writetofile kirjoittaa coins.txt -tiedostossa olevat kolikot erillisin tiedostoihin kolikko+prices.txt muodossa
+#tulostaa kolikon nimen, hinnan(usd) ja ajan
 def writetofile(coin, tradedcoin):
-    coinname = coin+"prices.txt"
+    coinname = coin+'prices.txt'
     cointext = open(coinname, "a")
     if(os.path.getsize(coinname) > 0):
             cointext.write("\n"+str(cg.get_price(ids=coin, vs_currencies=tradedcoin)) + " "+ t_string)
@@ -28,9 +31,11 @@ def writetofile(coin, tradedcoin):
     else:
             cointext.write(str(cg.get_price(ids=coin, vs_currencies=tradedcoin)) + " "+ t_string)
             cointext.close()
-
-
-writetofile(bitcoin, usd)
-writetofile(litecoin, usd)
-
+#avaa coins.txt tiedoston ja poistaa newlinet
+temp = open("coins.txt",'r').read().split('\n')
+#kay lapi kolikot
+for line in temp:
+    writetofile(line, usd)
+#writetofile(bitcoin, usd)
+#writetofile(litecoin, usd)
 writecoins()
